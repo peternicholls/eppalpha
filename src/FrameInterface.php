9e9d4221f9835f096d87f0205bdbfc8e97eed6b0
Peter Nicholls <nicholls@mac.com>
2017-10-26 22:07:22 +0100

    Project rename

---
+++ src/FrameInterface.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ *  PhpStorm
+ *
+ *  Use this section to define what this class is doing, the PHPDocumentator will use this
+ *  to automatically generate an API documentation using this information.
+ *
+ * @author peternicholls
+ * @date 26/10/2017 22:00
+ */
+
+namespace PeterNicholls\EPP;
+
+class FrameInterface {
+
+}
